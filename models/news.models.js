7f74fd10cfe2a2afdc71accf68aa4d8c8c6b59b8
Ben Bushell <benbushell@Bens-MacBook-Air.local>
2022-09-09 15:50:59 +0100

    update so landing page is /api

--- models/news.models.js
+++ models/news.models.js
@@ -2,7 +2,7 @@ const db = require('../db/connection');
 const { checkExists } = require('../db/seeds/utils');
 const fs = require('fs');
 const { Cipher } = require('crypto');
-const endpoints = require('../public/endpoints.json');
+const endpoints = require('../endpoints.json');
 
 exports.selectTopics = () => {
   let queryStr = `SELECT * FROM topics;`;
